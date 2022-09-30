a7384229f5b19b1755a8b8c3acace096a5bc7abb
jrhauser <jrhauser1127@gmail.com>
2022-09-30 18:02:33 -0400

    maybe this time

--- src/unit/Parser.java
+++ src/unit/Parser.java
@@ -6,16 +6,16 @@ import framework.TokenName;
 
 public class Parser {
 	public static void main(String[] args) {
-		String sentence = "let P = 1, let Q = 0, eval P -> Q?";
+		String sentence = "eval 1?";
 		boolean value = new Parser().analyze(sentence);
 		System.out.println(value);
 	}
 
 	Queue<Token> tokens;
-	
+	HashMap<Token, Boolean> lookup;
+
 	public boolean analyze(String input) {
 		tokens = new Lexer().tokenize(input);
-		
 		boolean value = program();
 		expect(TokenName.END_OF_INPUT);
 		
@@ -43,7 +43,6 @@ public class Parser {
 	}
 	
 	private boolean program() {
-		HashMap<Token, Boolean> lookup;
 		boolean value = evaluation();
 		while (peek(TokenName.LET_KEYWORD) || peek(TokenName.EVAL_KEYWORD)) {
 			if (accept(TokenName.LET_KEYWORD))
@@ -56,21 +55,56 @@ public class Parser {
 	}
 
 	private boolean evaluation() {
-		if (peek(TokenName.EVAL_KEYWORD)) {
-			
-		}
+		accept(TokenName.EVAL_KEYWORD);
 		boolean value = equivalance();
-
+		accept(TokenName.QUESTION);
 		return value;
 	}
 
 	private boolean assignment() {
-		
-		return false;
+		expect(TokenName.IDENTIFIER);
+		boolean value = (Boolean) tokens.element().value;
+		lookup.put(tokens.element(), value);
+		return value;
 	}
 
 	private boolean equivalance() {
-		return false;
+		boolean value = implication();
+		return value;
 	}
 
+	private boolean implication() {
+		boolean value = disjunction();
+		return value;
+	}
+	
+	private boolean disjunction() {
+		boolean value = conjunction();
+		return value;
+	}
+	
+	private boolean conjunction() {
+		boolean value = negation();
+		return value;
+	}
+
+	private boolean negation() {
+		boolean value = expression();
+		return value;
+	}
+	
+	private boolean expression() {
+		boolean value = boolean();
+		return value;
+	}
+
+	private boolean myBoolean() { 
+		boolean value = literal();
+		return value;
+	}
+
+	private boolean literal() {
+		boolean value = tokens.element().value;
+		return value;
+	}
 }
