403080148e0123bb6a45ebbb1b4493b0b6908ffb
jrhauser <jrhauser1127@gmail.comgit config --global user.name jrhausergit config --global user.email jrhauser1127@gmail.com>
2022-09-30 15:47:04 -0400

    Added helper method and the starting production

--- src/unit/Parser.java
+++ src/unit/Parser.java
@@ -1,4 +1,8 @@
 package unit;
+import java.util.HashMap;
+import java.util.Queue;
+import framework.Token;
+import framework.TokenName;
 
 public class Parser {
 	public static void main(String[] args) {
@@ -7,7 +11,66 @@ public class Parser {
 		System.out.println(value);
 	}
 
+	Queue<Token> tokens;
+	
 	public boolean analyze(String input) {
-		return false; // TODO implement stub
+		tokens = new Lexer().tokenize(input);
+		
+		boolean value = program();
+		expect(TokenName.END_OF_INPUT);
+		
+		return value;
 	}
+
+	private boolean accept(TokenName name) {
+		if (tokens.peek().name != name)
+			return false;
+		
+		tokens.remove();
+		return true;
+	}
+	
+	private boolean peek(TokenName name ) {
+		return tokens.peek().name == name;
+	}
+
+	private Object expect(TokenName name) {
+		if (tokens.peek().name != name)
+			throw new RuntimeException("Expected: " + name + 
+			"but found: " + tokens.peek().name);
+		
+			return tokens.remove().value;
+	}
+	
+	private boolean program() {
+		HashMap<Token, Boolean> lookup;
+		boolean value = evaluation();
+		while (peek(TokenName.LET_KEYWORD) || peek(TokenName.EVAL_KEYWORD)) {
+			if (accept(TokenName.LET_KEYWORD))
+				value = assignment();
+			else if (accept(TokenName.EVAL_KEYWORD))
+				value = equivalance();
+		}
+
+		return value;
+	}
+
+	private boolean evaluation() {
+		if (peek(TokenName.EVAL_KEYWORD)) {
+			
+		}
+		boolean value = equivalance();
+
+		return value;
+	}
+
+	private boolean assignment() {
+		
+		return false;
+	}
+
+	private boolean equivalance() {
+		return false;
+	}
+
 }
