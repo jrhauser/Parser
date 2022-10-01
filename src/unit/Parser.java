ecfeba82b0740c2ea16ea68bcc85c6dcf8ac0fd6
jrhauser <jrhauser1127@gmail.com>
2022-10-01 11:40:33 -0400

    Finished up most of it

--- src/unit/Parser.java
+++ src/unit/Parser.java
@@ -1,18 +1,19 @@
 package unit;
 import java.util.HashMap;
 import java.util.Queue;
+
 import framework.Token;
 import framework.TokenName;
 
 public class Parser {
 	public static void main(String[] args) {
-		String sentence = "eval 1?";
+		String sentence = "let aaa = 1, let BBB = 0, eval aaa -> BBB <-> BBB' -> aaa'?";
 		boolean value = new Parser().analyze(sentence);
 		System.out.println(value);
 	}
 
 	Queue<Token> tokens;
-	HashMap<Token, Boolean> lookup;
+	HashMap<String, Boolean> lookupTable = new HashMap<String,Boolean>();
 
 	public boolean analyze(String input) {
 		tokens = new Lexer().tokenize(input);
@@ -37,74 +38,170 @@ public class Parser {
 	private Object expect(TokenName name) {
 		if (tokens.peek().name != name)
 			throw new RuntimeException("Expected: " + name + 
-			"but found: " + tokens.peek().name);
+			" but found: " + tokens.peek().name);
 		
 			return tokens.remove().value;
 	}
 	
 	private boolean program() {
-		boolean value = evaluation();
+		boolean value = equivalance();
 		while (peek(TokenName.LET_KEYWORD) || peek(TokenName.EVAL_KEYWORD)) {
-			if (accept(TokenName.LET_KEYWORD))
-				value = assignment();
+			if (accept(TokenName.LET_KEYWORD)) {
+				assignment(lookupTable);
+			}
 			else if (accept(TokenName.EVAL_KEYWORD))
-				value = equivalance();
+				value = evaluation();
 		}
-
 		return value;
 	}
 
 	private boolean evaluation() {
-		accept(TokenName.EVAL_KEYWORD);
 		boolean value = equivalance();
-		accept(TokenName.QUESTION);
+		expect(TokenName.QUESTION);
 		return value;
 	}
 
-	private boolean assignment() {
-		expect(TokenName.IDENTIFIER);
-		boolean value = (Boolean) tokens.element().value;
-		lookup.put(tokens.element(), value);
-		return value;
+	private void assignment(HashMap<String, Boolean> lookup) {
+		while (!peek(TokenName.COMMA)) {
+			String identifier = (String) tokens.element().value;
+			expect(TokenName.IDENTIFIER);
+			expect(TokenName.EQUAL);
+			
+			boolean value = equivalance();
+		
+			if (lookup.get(identifier) != null) {
+					throw new RuntimeException("Variable already exists");
+			} 
+			
+			if (lookup.size() >= 8) {
+				throw new RuntimeException("The maximum number of variables has been reached");
+			}  
+			
+			lookup.put(identifier, value);
+		}
+		accept(TokenName.COMMA);
 	}
 
 	private boolean equivalance() {
 		boolean value = implication();
+		if (peek(TokenName.DOUBLE_ARROW)) {
+			while (!(peek(TokenName.QUESTION) || peek(TokenName.CLOSE_PAREN))) {
+				accept(TokenName.DOUBLE_ARROW);
+				if (peek(TokenName.OPEN_PAREN)) {
+					value = value == expression();
+				} else if (peek(TokenName.BOOL_LITERAL)){
+					boolean rightvalue = implication();
+					value = value == rightvalue;;	
+				} else if (peek(TokenName.IDENTIFIER)) {
+					boolean rightvalue = implication();
+					value = value == rightvalue;
+				} else {
+					throw new RuntimeException("AHHH");
+				}
+			}
+		}
 		return value;
 	}
 
 	private boolean implication() {
 		boolean value = disjunction();
+		if (peek(TokenName.ARROW)) {
+			while (!(peek(TokenName.QUESTION) || peek(TokenName.CLOSE_PAREN))) { 
+				accept(TokenName.ARROW);
+				if (peek(TokenName.OPEN_PAREN)) {
+					value = !value || expression();
+				} else if (peek(TokenName.BOOL_LITERAL)){
+					boolean rightvalue = disjunction();
+					value = !value || rightvalue;;	
+				} else if (peek(TokenName.IDENTIFIER)) {
+					boolean rightvalue = disjunction();
+					value = !value || rightvalue;
+				} else {
+					equivalance();
+				}
+			}
+		}
 		return value;
 	}
 	
 	private boolean disjunction() {
 		boolean value = conjunction();
+		if (peek(TokenName.VEE)) {
+			while (!(peek(TokenName.QUESTION) || peek(TokenName.CLOSE_PAREN))) {
+				accept(TokenName.VEE);
+				if (peek(TokenName.OPEN_PAREN)) {
+					value = value || expression();
+				} else if (peek(TokenName.BOOL_LITERAL)){
+					boolean rightvalue = conjunction();
+					value = value || rightvalue;;	
+				} else if (peek(TokenName.IDENTIFIER)) {
+					boolean rightvalue = conjunction();
+					value = value || rightvalue;
+				} else {
+					equivalance();
+				}
+			}
+		}
 		return value;
 	}
 	
 	private boolean conjunction() {
 		boolean value = negation();
+		if (peek(TokenName.CARET)) {
+			while (!(peek(TokenName.QUESTION) || peek(TokenName.CLOSE_PAREN))) {
+				accept(TokenName.CARET);
+				if (peek(TokenName.OPEN_PAREN)) {
+					value = value && expression();
+				} else if (peek(TokenName.BOOL_LITERAL)){
+					boolean rightvalue = negation();
+					value = value && rightvalue;;	
+				} else if (peek(TokenName.IDENTIFIER)) {
+					boolean rightvalue = negation();
+					value = value && rightvalue;
+				} else {
+					throw new RuntimeException("EXPECTED SOMETHING AFTER THE ^");
+				}
+			}
+		}
 		return value;
 	}
 
 	private boolean negation() {
 		boolean value = expression();
+		if (peek(TokenName.APOSTROPHE)) {
+			accept(TokenName.APOSTROPHE);
+			value = !(value);
+		}
 		return value;
 	}
 	
 	private boolean expression() {
-		boolean value = myBoolean();
+		boolean value;
+		if (peek(TokenName.OPEN_PAREN)) {
+			accept(TokenName.OPEN_PAREN);
+			value = equivalance();
+			expect(TokenName.CLOSE_PAREN);
+		} else {
+			value = myBoolean();
+		}
 		return value;
 	}
 
-	private boolean myBoolean() { 
-		boolean value = literal();
-		return value;
+	private boolean myBoolean() {
+		if (peek(TokenName.BOOL_LITERAL)) {
+			boolean value = literal();
+			return value;
+		} else if (peek(TokenName.IDENTIFIER)) {
+			boolean value = lookupTable.get((String) tokens.element().value);
+			accept(TokenName.IDENTIFIER);
+			return value;
+		}
+		return false;
 	}
 
 	private boolean literal() {
-		boolean value = true;
+		boolean value = (Boolean) tokens.element().value;
+		accept(TokenName.BOOL_LITERAL);
 		return value;
 	}
 }
